f1860cfd566a3a5b557220eb4f9cc4412903920e
gerousdan <fostiake@gmail.com>
2021-06-21 22:30:02 +0100

    backgrounds

--- script.js
+++ script.js
@@ -3,13 +3,35 @@ window.onload = function () {
   var token =
     "eyJhbGciOiJFUzI1NiIsImtpZCI6IllycWVMVHE4ei1vZkg1bnpsYVNHbllSZkI5YnU5eVBsV1lVXzJiNnFYT1EifQ.eyJpZCI6IlI5SllZNFdQIiwib3BuIjoiVGVzdCBRUiBwb3N0ZXIgZm9yIFdhbGVzIiwidnQiOiIwMTIiLCJwYyI6IkNGMjQ0VE4ifQ.AKTiwwXLAx6rDkxDhVbRncM1uhOchpJqBAZ-PjY1IAjW3Wihi8ji-sQ-mFbzjhLL5mnrz5Ep8d7MnOHkf0MBEQ";
 
+  const fonts = [14, 15, 16, 17, 19, 21, 23, 28, 33, 40, 47, 53];
+  document.body.style.backgroundImage =
+    "url('" +
+    (1 +
+      fonts.indexOf(
+        parseInt(window.getComputedStyle(document.body).fontSize)
+      )) +
+    ".PNG')";
+
   document.getElementById("home").onclick = function () {
     document.getElementById("venue").innerText = window.prompt(
       "",
       parseJwt(token)
     );
   };
-
+  /* text (4=default, 8+): px
+1: 14
+2: 15
+3: 16
+4: 17
+5: 19
+6: 21
+7: 23
+8: 28
+9: 33
+10:40
+11:47
+12:53
+ */
   document.getElementById("top").onclick = function () {
     var newVenue = document.getElementById("venue").innerText;
     var newTime = document.getElementById("time").innerText;
